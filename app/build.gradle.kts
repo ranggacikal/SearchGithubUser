2e600a8e59e40cb619002f0e9ee9ceefa3aa95e5
ranggacikal <ranggacikal2@gmail.com>
2025-07-29 22:58:36 +0700

    adding hilt application and changes gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.library)
 }
 
 android {
@@ -33,6 +34,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    packaging {
+        resources {
+            excludes += "META-INF/gradle/incremental.annotation.processors"
+        }
+    }
 }
 
 dependencies {
@@ -45,4 +51,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(project(":feature-user"))
+    implementation(project(":api"))
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.compiler)
 }
